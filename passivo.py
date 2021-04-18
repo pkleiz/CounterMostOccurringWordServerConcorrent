130883bf4be5aecd61fc150513d5304e02357568
pkleiz <57454126+pkleiz@users.noreply.github.com>
2021-04-18 17:45:33 -0300

    Add files via upload

--- passivo.py
+++ passivo.py
@@ -1,47 +1,106 @@
-# Exemplo basico socket (lado passivo)
-
+#servidor de echo: lado servidor
+#com finalizacao do lado do servidor
+#com multithreading (usa join para esperar as threads terminarem apos digitar 'fim' no servidor)
 import socket
+import select
+import sys
+import threading
 from collections import Counter
 
-HOST = ''    # '' possibilita acessar qualquer endereco alcancavel da maquina local
-PORTA = 5000  # porta onde chegarao as mensagens para essa aplicacao
+# define a localizacao do servidor
+HOST = '' # vazio indica que podera receber requisicoes a partir de qq interface de rede da maquina
+PORT = 5000 # porta de acesso
 
-# cria um socket para comunicacao
-sock = socket.socket() # valores default: socket.AF_INET, socket.SOCK_STREAM  
+#define a lista de I/O de interesse (já inclui a entrada padrao)
+entradas = [sys.stdin]
+#armazena historico de conexoes 
+conexoes = {}
 
-# vincula a interface e porta para comunicacao
-sock.bind((HOST, PORTA))
+def iniciaServidor():
+    '''Cria um socket de servidor e o coloca em modo de espera por conexoes
+    Saida: o socket criado'''
+    # cria o socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Internet( IPv4 + TCP) 
 
-# define o limite maximo de conexoes pendentes e coloca-se em modo de espera por conexao
-sock.listen(5) 
+    # vincula a localizacao do servidor
+    sock.bind((HOST, PORT))
 
-# aceita a primeira conexao da fila (chamada pode ser BLOQUEANTE)
-novoSock, endereco = sock.accept() # retorna um novo socket e o endereco do par conectado
-print ('Conectado com: ', endereco)
+    # coloca-se em modo de espera por conexoes
+    sock.listen(5) 
 
+    # configura o socket para o modo nao-bloqueante
+    sock.setblocking(False)
 
+    # inclui o socket principal na lista de entradas de interesse
+    entradas.append(sock)
 
-while True:
-    # depois de conectar-se, espera uma mensagem (chamada pode ser BLOQUEANTE))
-    msg = novoSock.recv(1024)
-    a = msg.decode()
-    remove_characters = [".", ",","'","!",":",";","(",")","[","]","{","}","@"]
-    
-    for character in remove_characters:
-        a = a.replace(character, "")
-    
-    a = a.lower().split()
-    # conta as palavras mais comuns
-    z=Counter(a).most_common()[:5]
-    z = str(z)
-    z = z.encode()
-    if not z:
-        break
-    else:
-        novoSock.send(z)
+    return sock
 
-# fecha o socket da conexao
-novoSock.close() 
+def aceitaConexao(sock):
+    '''Aceita o pedido de conexao de um cliente
+    Entrada: o socket do servidor
+    Saida: o novo socket da conexao e o endereco do cliente'''
 
-# fecha o socket principal
-sock.close()
+    # estabelece conexao com o proximo cliente
+    clisock, endr = sock.accept()
+
+    # registra a nova conexao
+    conexoes[clisock] = endr 
+
+    return clisock, endr
+
+def atendeRequisicoes(clisock, endr):
+    '''Recebe mensagens e as envia de volta para o cliente (ate o cliente finalizar)
+    Entrada: socket da conexao e endereco do cliente
+    Saida: '''
+
+    while True:
+        #recebe dados do cliente
+        data = clisock.recv(1024)
+        if not data: # dados vazios: cliente encerrou
+            print(str(endr) + '-> encerrou')
+            clisock.close() # encerra a conexao com o cliente
+            return
+        a = data.decode()
+        remove_characters = [".", ",","'","!",":",";","(",")","[","]","{","}","@"]
+        
+        for character in remove_characters:
+            a = a.replace(character, "")
+        
+        a = a.lower().split()
+        # conta as palavras mais comuns
+        z=Counter(a).most_common()[:10]
+        z = str(z)
+        z = z.encode('utf-8')
+        if not z:
+            break
+        else:
+            clisock.send(z) # envia os dados tratados para o cliente
+
+def main():
+	'''Inicializa e implementa o loop principal (infinito) do servidor'''
+	clientes=[] #armazena as threads criadas para fazer join
+	sock = iniciaServidor()
+	print("Pronto para receber conexoes...")
+	while True:
+		#espera por qualquer entrada de interesse
+		leitura, escrita, excecao = select.select(entradas, [], [])
+		#tratar todas as entradas prontas
+		for pronto in leitura:
+			if pronto == sock:  #pedido novo de conexao
+				clisock, endr = aceitaConexao(sock)
+				print ('Conectado com: ', endr)
+				#cria nova thread para atender o cliente
+				cliente = threading.Thread(target=atendeRequisicoes, args=(clisock,endr))
+				cliente.start()
+				clientes.append(cliente) #armazena a referencia da thread para usar com join()
+			elif pronto == sys.stdin: #entrada padrao
+				cmd = input()
+				if cmd == 'fim': #solicitacao de finalizacao do servidor
+					for c in clientes: #aguarda todas as threads terminarem
+						c.join()
+					sock.close()
+					sys.exit()
+				elif cmd == 'hist': #outro exemplo de comando para o servidor
+					print(str(conexoes.values()))
+main()
